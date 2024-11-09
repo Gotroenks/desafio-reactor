c5b8317d12f03896906d55e846b2e5d1fdac84a5
Gotroenks <isaacmoura_gerboni@hotmail.com>
2024-11-09 00:44:41 -0300

    Complete the challenge basic webFlux

--- src/main/java/br/com/dio/desafioreactor/Question2.java
+++ src/main/java/br/com/dio/desafioreactor/Question2.java
@@ -9,6 +9,15 @@ public class Question2 {
     /*
     Recebe uma lista de usuários e retorna a quantos usuários admin tem na lista
      */
-    public Mono<Long> countAdmins(final List<User> users){}
+    public Mono<Long> countAdmins(final List<User> users){
+        Long admins = users.stream()
+                .filter(User::isAdmin)
+                .count();
+
+        return Mono.justOrEmpty(admins)
+                .switchIfEmpty(Mono.defer(() ->
+                        Mono.error(new NullPointerException("Lista vazia")))
+                );
+    }
 
 }
